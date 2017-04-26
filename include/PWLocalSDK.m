f42c4c0c91de7035de6a98e0ced591e4f58bce25
tien.vu <tien@paymentwall.com>
2017-04-26 18:28:21 +0700

    fix non module pod

---
+++ include/PWLocalSDK.m
@@ -0,0 +1,8 @@
+//
+//  PWCoreSDK.m
+//  PWCoreSDK
+//
+//  Created by Tien Nhat Vu on 4/26/17.
+//  Copyright © 2017 Paymentwall. All rights reserved.
+//
+// This file exists so that CocoaPods will generate a modulemap for vendored_libraries
