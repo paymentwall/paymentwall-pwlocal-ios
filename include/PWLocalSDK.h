f6613fc0f8d373060cfb1e8270f8f04c50c400f0
tien.vu <tien@paymentwall.com>
2017-04-26 18:49:52 +0700

    fix non module pod

--- include/PWLocalSDK.h
+++ include/PWLocalSDK.h
@@ -2,44 +2,16 @@
 //  PWLocalSDK.h
 //  PWLocalSDK
 //
-//  Created by Tien Nhat Vu on 4/18/17.
+//  Created by Tien Nhat Vu on 4/26/17.
 //  Copyright © 2017 Paymentwall. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
+// Header file
 #import "PWLocalResponse.h"
-
-@protocol PWLocalSDKDelegate <NSObject>
-
-@optional
--(void) pwLocalResponse:(PWLocalResponse* _Nonnull) response;
-
-@end
-
-@interface PWLocalSDK : NSObject
-
-+(void)showPWLocalViewControllerWithViewController:(UIViewController * _Nonnull)parentViewController params:(id _Nonnull)params secretKey:(NSString * _Nullable)secretKey downloadURL:(NSString * _Nullable)downloadURL;
-
-// Check payment status
-// SUPPORT DIGITAL GOODS FLEXIBLE WIDGET ONLY
-//+(void)checkPaymentStatusWithKey:(NSString *) key agExternalId:(NSString*) agExternalId uid:(NSString*) uid signVersion:(int)signVersion andSecretKey:(NSString*) secret completion:(PWLocalPaymentStatusCompletionBlock)block;
-
-/**
- Extra headers required to process PWLocal, get this to add into your request if you are going to use your own webview, included:
- 
- HTTP_X_APP_NAME: app name
- HTTP_X_REQUESTED_WITH: bundle identifier
- HTTP_X_DOWNLOAD_LINK: app download url, default is "url"
- */
-
-+(NSDictionary *_Nonnull) getExtraHeaders;
-
-/**
- Create string to sign from your params, if you don't use "success_url", default will be "pwlocal://paymentsuccessful", you can track this value on your webview's "shouldStartLoadWithRequest" or similar to close webview if you decide to use your own webview
- 
- (id)params: Can be Dictionary or object: DigitalGoodsDefautWidget / DigitalGoodsFlexibleWidget / VirtualCurrency / CartDefaultWidget
- */
-+(NSString *_Nonnull) getStringToSign:(id _Nonnull)params;
-
-@end
+#import "CartDefaultWidget.h"
+#import "DigitalGoodsDefautWidget.h"
+#import "DigitalGoodsFlexibleWidget.h"
+#import "VirtualCurrency.h"
+#import "Subscription.h"
+#import "PWLocalStatusResponse.h"
+#import "PWLocalSDKMain.h"
